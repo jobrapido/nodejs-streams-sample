4f2dc88a9766c8d325ed32205ccdd75f6edd927f
Howard Scordio <howard.scordio@jobrapido.com>
2019-10-25 16:49:27 +0200

    formatting files and launch json

--- test/service/gender-assigner-pipeline.test.ts
+++ test/service/gender-assigner-pipeline.test.ts
@@ -1,10 +1,10 @@
-import {GenderAssignerPipeline} from "../../src/service/gender-assigner-pipeline"
 import { Mock, Times } from "typemoq";
 import { ApplicationConfig } from "../../src/config";
-import { InputDecoderTransformStream } from "../../src/stream/input-decoder";
-import { BufferTransformStream } from "../../src/stream/buffer-stream";
-import { AssignGenderTransformStream } from "../../src/stream/assign-gender";
+import {GenderAssignerPipeline} from "../../src/service/gender-assigner-pipeline";
 import { GenderizeAPI } from "../../src/service/genderize-api";
+import { AssignGenderTransformStream } from "../../src/stream/assign-gender";
+import { BufferTransformStream } from "../../src/stream/buffer-stream";
+import { InputDecoderTransformStream } from "../../src/stream/input-decoder";
 import { TestInputStream, TestOutStream } from "../test-stream";
 
 describe("Gender assigner module", () => {
@@ -13,30 +13,31 @@ describe("Gender assigner module", () => {
 
     const configs = Mock.ofType(ApplicationConfig);
     const genderizeApi = Mock.ofType(GenderizeAPI);
-    const resultName1 = {name: "name1",gender:"male",probability:"0.99"};
-    const resultName2 = {name: "name2",gender:"female",probability:"0.59"};
-    const resultName3 = {name: "name3",gender:"male",probability:"0.99"};
-    const resultName4 = {name: "name4",gender:"female",probability:"0.59"};
+    const resultName1 = {name: "name1", gender: "male", probability: "0.99"};
+    const resultName2 = {name: "name2", gender: "female", probability: "0.59"};
+    const resultName3 = {name: "name3", gender: "male", probability: "0.99"};
+    const resultName4 = {name: "name4", gender: "female", probability: "0.59"};
 
     genderizeApi.setup((instance) => instance
       .genderize("name1"))
       .returns(() => Promise.resolve(resultName1));
+
     genderizeApi.setup((instance) => instance
-    .genderize("name2"))
-    .returns(() => Promise.resolve(resultName2));
+      .genderize("name2"))
+      .returns(() => Promise.resolve(resultName2));
     genderizeApi.setup((instance) => instance
-    .genderize("name3"))
-    .returns(() => Promise.resolve(resultName3));
+      .genderize("name3"))
+      .returns(() => Promise.resolve(resultName3));
     genderizeApi.setup((instance) => instance
-    .genderize("name4"))
-    .returns(() => Promise.resolve(resultName4));
+      .genderize("name4"))
+      .returns(() => Promise.resolve(resultName4));
 
     const inputDecoder = new InputDecoderTransformStream();
     const bufferTransformStream = new BufferTransformStream(configs.object);
     const assignGenderTransformStream = new  AssignGenderTransformStream(genderizeApi.object);
 
-    const chunk1 = `name1\nname2\nname3\nname4\n`;
-    const inputStream = TestInputStream.fromBuffers(Buffer.from(chunk1));
+    const chunk = `name1\nname2\nname3\nname4\n`;
+    const inputStream = TestInputStream.fromBuffers(Buffer.from(chunk));
     inputStream.pipe(process.stdout);
 
     const outputStream = TestOutStream.asBufferedStream<Buffer>();
