8279ecebda6d5309f56940e94e12ded6046afd69
StefanoRanzini <stefano.ranzini@jobrapido.com>
2019-11-27 14:52:56 +0100

    Injectable csv parser

--- src/gender-assigner-pipeline.ts
+++ src/gender-assigner-pipeline.ts
@@ -7,49 +7,50 @@ import { logger } from "./logger";
 import { AssignGenderTransformStream } from "./stream/assign-gender";
 import { BufferTransformStream } from "./stream/buffer-stream";
 import { InputDecoderTransformStream } from "./stream/input-decoder";
-import {MetricStream} from "./stream/metric-stream";
+import { MetricStream } from "./stream/metric-stream";
 
 @injectable()
 export class GenderAssignerPipeline {
-
   constructor(
     private readonly configs: ApplicationConfig,
     private readonly inputDecoder: InputDecoderTransformStream,
     private readonly bufferTransformStream: BufferTransformStream,
     private readonly assignGenderTransformStream: AssignGenderTransformStream,
+    @inject("csv:parser") private readonly csvParser: Parser,
     @inject("fs:input") private readonly fsInput: Readable,
-    @inject("fs:output") private readonly fsOutput: Writable,
-  ) { }
+    @inject("fs:output") private readonly fsOutput: Writable
+  ) {}
 
   public assignGender() {
     return new Promise(async (resolve, reject) => {
-
-      const csvParser = new Parser({ trim: true });
-
       const {
         LOCAL_INPUT_FILE_NAME,
         LOCAL_OUTPUT_FILE_NAME,
-        SAMPLE_SIZE,
+        SAMPLE_SIZE
       } = this.configs;
 
       try {
-        logger.info(`Starting assign gender pipeline [in=${LOCAL_INPUT_FILE_NAME}, out=${LOCAL_OUTPUT_FILE_NAME}]`);
-        this
-          .fsInput
-          .pipe(csvParser)
+        logger.info(
+          `Starting assign gender pipeline [in=${LOCAL_INPUT_FILE_NAME}, out=${LOCAL_OUTPUT_FILE_NAME}]`
+        );
+        this.fsInput
+          .pipe(this.csvParser)
           .pipe(this.inputDecoder)
           .pipe(this.bufferTransformStream)
           .pipe(this.assignGenderTransformStream)
           .pipe(new Stringifier({}))
           .pipe(new MetricStream(SAMPLE_SIZE))
           .on("metrics", (throughput: number) => {
-            logger.info(`Current metrics throughput: ${throughput} elements/sec`);
+            logger.info(
+              `Current metrics throughput: ${throughput} elements/sec`
+            );
           })
-          .pipe(this.fsOutput
-            .on("close", async () => {
+          .pipe(
+            this.fsOutput.on("close", async () => {
               logger.info("Finished");
               resolve();
-            }));
+            })
+          );
       } catch (e) {
         reject(e);
       }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,48 @@
 #!/usr/bin/env node
 import { Container, Provider, Scope } from "@msiviero/knit";
+import { Parser } from "csv-parse";
 import * as fs from "fs";
 import { RestClient } from "typed-rest-client";
-import {Application} from "./app";
+import { Application } from "./app";
 
-Container
-    .getInstance()
-    .registerProvider("fs:input", class implements Provider<fs.ReadStream> {
-    public provide = () => fs.createReadStream("data/input/input.csv");
-    }, Scope.Singleton)
-    .registerProvider("fs:output", class implements Provider<fs.WriteStream> {
-    public provide = () => fs.createWriteStream("data/output/output.csv");
-    }, Scope.Singleton)
-    .registerProvider("rest:client", class implements Provider<RestClient> {
-    public provide = () => new RestClient("genderize-rest-client", undefined, undefined, {socketTimeout: 2000});
-    }, Scope.Singleton)
-    .registerProvider("config:search-limited", class implements Provider<boolean> {
-    public provide = () => true;
-    }, Scope.Singleton)
-    .resolve(Application)
-    .start();
+Container.getInstance()
+  .registerProvider(
+    "fs:input",
+    class implements Provider<fs.ReadStream> {
+      public provide = () => fs.createReadStream("data/input/input.csv");
+    },
+    Scope.Singleton
+  )
+  .registerProvider(
+    "fs:output",
+    class implements Provider<fs.WriteStream> {
+      public provide = () => fs.createWriteStream("data/output/output.csv");
+    },
+    Scope.Singleton
+  )
+  .registerProvider(
+    "rest:client",
+    class implements Provider<RestClient> {
+      public provide = () =>
+        new RestClient("genderize-rest-client", undefined, undefined, {
+          socketTimeout: 2000
+        });
+    },
+    Scope.Singleton
+  )
+  .registerProvider(
+    "csv:parser",
+    class implements Provider<Parser> {
+      public provide = () => new Parser({ trim: true });
+    },
+    Scope.Singleton
+  )
+  .registerProvider(
+    "config:search-limited",
+    class implements Provider<boolean> {
+      public provide = () => true;
+    },
+    Scope.Singleton
+  )
+  .resolve(Application)
+  .start();
